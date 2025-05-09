9dc8a9b1bda0d54d3566c092b607067a0f4ebdad
nascimento3322 <vn62511@gmail.com>
2025-05-09 20:28:27 -0300

    Multiplicando

--- code.py
+++ code.py
@@ -1,10 +1,5 @@
-#Somando e Subtraindo 
-y = int(input("Começaremos na soma, então digite um número: "))
-z = int(input("Continuando na soma, digite outro número: "))
-soma = y + z 
-print(f"O resultado final é: {soma}")
-
-a = int(input("Na subtração, digite um número: "))
-b = int(input("Continuando na subtração digite mais um número: "))
-subtração = a - b
-print(f"A subtração final é: {subtração}")
+#Multiplicação 
+w = int(input("Entramos na multiplicação, então digite um número: "))
+k = int(input("Após isso, digite outro número: "))
+multiplicação = w * k
+print(f"O resultado final é: {multiplicação}")
