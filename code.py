50f79eaa5d856151a6dc7fbc27bb1a83cf9504d3
nascimento3322 <vn62511@gmail.com>
2025-05-09 20:37:08 -0300

    Update code.py

--- code.py
+++ code.py
@@ -13,4 +13,10 @@ b = int(input("Digite mais um número: "))
 subtração = a - b
 print(f"A subtração final é: {subtração}")
 
+#Multiplicação 
+w = int(input("Entramos na multiplicação, então digite um número: "))
+k = int(input("Após isso, digite outro número: "))
+multiplicação = w * k
+print(f"O resultado final é: {multiplicação}")
+
 
