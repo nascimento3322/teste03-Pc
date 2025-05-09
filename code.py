13363cc835bf2eb38075121e3c3462822296dfb8
nascimento3322 <vn62511@gmail.com>
2025-05-09 19:19:32 -0300

    Subtraindo

--- code.py
+++ code.py
@@ -1,1 +1,5 @@
-print("Está é uma calculadora")
+print("Subtração")
+a = int(input("Digite um número: "))
+b = int(input("Digite mais uma número: "))
+subtração = a - b
+print(f"A subtração final é: {subtração}")
