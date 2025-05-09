0e03eea88459d3c62387223568a1b31e79d60d43
nascimento3322 <vn62511@gmail.com>
2025-05-09 20:35:30 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,4 @@
-# Subtração
-a = int(input("Digite um número: "))
-b = int(input("Digite mais um número: "))
-subtração = a - b
-print(f"A subtração final é: {subtração}")
+print("Está é uma calculadora")
 
 # Soma
 print("Somando")
@@ -10,3 +6,11 @@ x = int(input("Digite um número: "))
 y = int(input("Digite outro número: "))
 soma = x + y
 print(f"O resultado final é: {soma}")
+
+# Subtração
+a = int(input("Digite um número: "))
+b = int(input("Digite mais um número: "))
+subtração = a - b
+print(f"A subtração final é: {subtração}")
+
+
